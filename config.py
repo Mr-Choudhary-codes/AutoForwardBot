4d90b44245913ed4b5b0f9844e06119938c94aef
Mr-Choudhary-codes <vickychaudhary2849@gmail.com>
2025-06-15 21:52:08 +0530

    Update config.py\

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", 7767099543).split()]
     BOT_SESSION = environ.get("BOT_SESSION", "BQGPUn8AHhvwMphLtw4enXLJtkqEZgtuijqf-oWVH3uYaRZztpjfhUUE8r9XDCocoWdvW0Vf_7MGw7yPwJ4t-zwL0XuU6MjLV6rfieurqBEtXFXKT6Tmj5f16vg0gQMYkFNH1U4gQnDbEOtgSdWzlhSKden4QLScYMG-0KPELbsee_bCqIufrPpgw2_JloMNZBx4a4bClQC0PDe7azKSd5w75Ks5qQrlME13aEPots8Tp6q4ejbZEPHsPnyRgRBSuahbYUbDggGvswhW7ZaIiCxaRw0Yv_P_uW0caxJnG8I7R3mMB0P3VD-XrLYuB-7MW2PETThqs5GWR2ZjbQ36qDYyUlyhMwAAAAHG7fs5AA")
 
-    PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
+    PICS = (environ.get('PICS', 'https://graph.org/file/fd5ce10d938e2e6c67f58-3fbde42385e9cc0792.jpg'))
     
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://KingOfHell:Highspeedorg@cluster0.ha2cc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
